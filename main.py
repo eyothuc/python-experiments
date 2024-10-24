d22a17152019178065f1e848db39103a2cad0e6d
LaJope <Siephan2.0.0.3@gmail.com>
2024-10-24 19:08:47 +0300

    Added functionality to get information about vehicle positions from vehicleID. Changed the json response format.

--- main.py
+++ main.py
@@ -2,23 +2,30 @@
 from flask import Flask, jsonify
 import gtfs_handlers
 
-
 app = Flask(__name__)
-
-
-@app.route('/api/stop-<id>', methods=['GET'])
-def get_stop_info(id):
-    vehicles = gtfs_handlers.get_stop_vehicles(id)
-    return vehicles
 
 
 @app.route('/api/stops', methods=['GET'])
 def get_stops():
     stops = gtfs_handlers.parse_feed(
-        f"./{gtfs_handlers.FEED_DIR}/stops.txt",
-        "stop_id", "stop_name", "stop_lat",
-        "stop_lon", "transport_type")
-    return jsonify(stops)
+        file_name='stops.txt',
+        ignore_prefix='stop_',
+        collect_data=['stop_id', 'stop_name', 'stop_lat',
+                      'stop_lon'])
+    return jsonify(stops[:10])
+
+
+@app.route('/api/stops/<id>', methods=['GET'])
+def get_stop_info(id):
+    stop_info = gtfs_handlers.get_stop_realtime_info(id)
+    return jsonify(stop_info)
+
+
+@app.route('/api/vehicles/<ids>', methods=['GET'])
+def get_vehicle_info(ids):
+    vehicle_info = gtfs_handlers.get_vehicle_realtime_info(ids)
+    gtfs_handlers.get_position_realtime_info()
+    return jsonify(vehicle_info)
 
 
 if (__name__ == '__main__'):
