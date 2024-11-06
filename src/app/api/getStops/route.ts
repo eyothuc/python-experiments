c1d767ae176c9a2310cf21904e56cafa61abb7f4
Maksim <maxprofix@gmail.com>
2024-11-06 17:57:50 +0300

    Initial commit

---
+++ src/app/api/getStops/route.ts
@@ -0,0 +1,20 @@
+// app/api/getStops/route.ts
+import { NextResponse } from "next/server";
+import axios from "axios";
+
+// Обработчик GET-запросов
+export async function GET() {
+  try {
+    // Делаем запрос на внешнее API
+    const response = await axios.get("https://trashapigtfs.loca.lt/api/stops");
+
+    // Возвращаем полученные данные как JSON
+    return NextResponse.json(response.data);
+  } catch (error) {
+    // Обрабатываем ошибки и возвращаем ошибочный статус
+    return NextResponse.json(
+      { message: "Ошибка при получении данных" },
+      { status: 500 }
+    );
+  }
+}
