b8ce93271e75bbd529ec19349300bf865a178bc0
LaJope <Siephan2.0.0.3@gmail.com>
2024-11-08 13:58:23 +0300

    Replaced func parse_csv_file with pandas.read_csv in update_db()

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 import tables
 import gtfs_handlers
+import pandas
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-
 
 
 ENGINE = \
@@ -15,50 +15,15 @@ def create_db():
 
 
 def update_db():
-    stops = gtfs_handlers.parse_csv_file(
-        file_name='stops.txt',
-        ignore_prefix='stop_',
-        collect_data='all')
+    file_name = f'{gtfs_handlers.FEED_DIR}/stops.txt'
+    dataframe = pandas.read_csv(file_name).set_index('stop_id')
+    dataframe.to_sql(con=ENGINE, name=tables.Stop.__tablename__,
+                     if_exists='replace')
 
-    routes = gtfs_handlers.parse_csv_file(
-        file_name='routes.txt',
-        ignore_prefix='route_',
-        collect_data='all'
-    )
-
-    with Session(ENGINE) as session:
-        session.query(tables.Stop).delete()
-        session.query(tables.Route).delete()
-        session.commit()
-
-        for stop in stops:
-            instance = tables.Stop(
-                stop_id=stop["id"],
-                stop_code=stop["code"],
-                stop_name=stop["name"],
-                stop_lat=stop["lat"],
-                stop_lon=stop["lon"],
-                location_type=stop["location_type"],
-                wheelchair_boarding=stop["wheelchair_boarding"],
-                transport_type=stop["transport_type"]
-            )
-            session.add(instance)
-
-        for route in routes:
-            instance = tables.Route(
-                route_id=route["id"],
-                agency_id=route["agency_id"],
-                route_short_name=route["short_name"],
-                route_long_name=route["long_name"],
-                route_type=route["type"],
-                transport_type=route["transport_type"],
-                circular=route["circular"],
-                urban=route["urban"],
-                night=route["night"]
-
-            )
-            session.add(instance)
-        session.commit()
+    file_name = f'{gtfs_handlers.FEED_DIR}/routes.txt'
+    dataframe = pandas.read_csv(file_name).set_index('route_id')
+    dataframe.to_sql(con=ENGINE, name=tables.Stop.__tablename__,
+                     if_exists='replace')
 
 
 def query_db(db_class):
