f12a77dbf373f7c3f87ab2b27c06e665bcec206f
LaJope <Siephan2.0.0.3@gmail.com>
2024-12-04 17:25:30 +0300

    Add stoplists functionality

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,40 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, session
 from flask_cors import CORS
-from flask_login import LoginManager, login_user, logout_user
+from flask_login import LoginManager, login_user, logout_user, current_user
+# from flask_login import login_required
+from functools import wraps
+
 from werkzeug.security import generate_password_hash, check_password_hash
 
 import database
 import gtfs_handlers
 import tables
 
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        print(request.cookies)
+        print(request.headers)
+        print(session)
+        if not current_user.is_authenticated:
+            return jsonify({'error': 'Unauthorized'}), 401
+        return f(*args, **kwargs)
+    return decorated_function
+
+
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
+# CORS(app, supports_credentials=True, origins=["https://your-frontend-domain.com"])
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SECRET_KEY'] = 'your_key'
-app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_PERMANENT'] = True
+
+app.config["SESSION_COOKIE_SAMESITE"] = "None"
+app.config["SESSION_COOKIE_SECURE"] = True
 
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+# login_manager.login_view = 'login'
 login_manager.init_app(app)
 
 
@@ -29,9 +49,11 @@ def register():
     username = data.get('username')
     password = data.get('password')
     if database.get_user_by_username(username):
-        return jsonify({"message": "Пользователь c таким логином уже существует"}), 401
+        return jsonify({"message":
+                        "Пользователь c таким логином уже существует"}), 401
     if not username or not password:
-        return jsonify({"message": "Поля username и password должны быть заполнены"}), 401
+        return jsonify({"message":
+                        "Поля username и password должны быть заполнены"}), 401
     password_hash = generate_password_hash(password)
     database.create_user(username, password_hash)
     return jsonify({"message": "Регистрация прошла успешно"}), 201
@@ -48,6 +70,7 @@ def login():
         if not check_password_hash(user.password, password):
             return jsonify({"message": "Неверные данные"}), 401
         login_user(user, remember=is_remember)
+        print(session)
         return jsonify({"message": "Успешный вход"}), 200
     return jsonify({"message": "Пользователь не найден"}), 401
 
@@ -80,6 +103,7 @@ def getRoutes():
 
 @app.route('/api/stops/<stop_id>', methods=['GET'])
 def getStopInfo(stop_id):
+    # return jsonify([{"route_id": "Hello, Makushimu"}])
     stop_info = gtfs_handlers.get_stop_forecast_realtime_info(stop_id)
     return jsonify(stop_info)
 
@@ -114,6 +138,97 @@ def create_db():
 def update_db():
     database.update_db()
     return jsonify("Updating")
+
+
+@app.route('/api/lists', methods=['GET'])
+@login_required
+def get_list():
+    return jsonify(current_user.get_lists_dict()), 200
+
+
+@app.route('/api/lists', methods=['POST'])
+@login_required
+def add_list():
+    data = request.json
+    stops = list(map(int, data.get('stops', [])))
+    name = data.get('name', None)
+    if name is None:
+        return jsonify("Error")
+    new_list = database.add_list(name, stops, current_user.user_id)
+
+    return jsonify(new_list.to_dict()), 200
+
+
+@app.route('/api/lists/<list_id>/add', methods=['POST'])
+# @login_required
+def add_stop_to_list(list_id):
+    data = request.json
+    username = data.get('username', '')
+    stops = int(data.get('stopId', 0))
+    if not stops or not username:
+        return 'No input'
+
+    res = database.add_stops_to_list(username,
+                                     int(list_id), stops)
+    if res:
+        return 'forbidden', 403
+    return jsonify(res), 200
+
+
+@app.route('/api/lists/<list_id>/remove', methods=['POST'])
+# @login_required
+def remove_stop_from_list(list_id):
+    data = request.json
+    username = data.get('username', '')
+    stop_id = int(data.get('stopId', 0))
+    if not stop_id or not username:
+        return 'No input'
+
+    res = database.remove_stops_from_list(username, int(list_id), stop_id)
+    if res:
+        return 'Forbidden', 403
+    return jsonify(res), 200
+
+
+@app.route('/api/lists/<list_id>', methods=['GET'])
+@login_required
+def get_list_by_id(list_id):
+    return jsonify(database.get_list_by_id(int(list_id)).to_dict())
+
+
+@app.route('/api/lists/user/<username>', methods=['GET'])
+def get_lists_by_username(username):
+    user = database.get_user_by_username(username)
+    if not user:
+        return jsonify({"message": "Пользователь не найден"}), 404
+    user_lists = database.get_lists_by_user_id(user.user_id)
+    lists = []
+    for lst in user_lists:
+        lists.append(dict())
+        lists[-1]["list_info"] = lst.to_dict(ignore_stops=False)
+        lists[-1]["stops"] = []
+        for stop in lst.stops:
+            lists[-1]["stops"].append(dict())
+            lists[-1]["stops"][-1] =\
+                gtfs_handlers.get_stop_forecast_realtime_info(stop.stop_id)
+    return jsonify(lists), 200
+
+
+@app.route('/api/lists/user/<username>', methods=['POST'])
+def add_list_to_user_by_username(username):
+    user = database.get_user_by_username(username)
+    if not user:
+        return jsonify({"message": "Пользователь не найден"}), 404
+
+    data = request.json
+    stops = list(map(int, data.get('stops', [])))
+    name = data.get('name', None)
+
+    if not name:
+        return jsonify({"message": "Имя списка (name) обязательно"}), 400
+
+    new_list = database.add_list(name, stops, user.user_id)
+    return jsonify(new_list.to_dict()), 201
 
 
 if __name__ == '__main__':
--- backend/tables.py
+++ backend/tables.py
@@ -1,10 +1,16 @@
-from typing import Optional
-
 from flask_login import UserMixin
+
+from typing import Optional, List
+
+from sqlalchemy import Column
+from sqlalchemy import Table
 from sqlalchemy import String
+from sqlalchemy import ForeignKey
+
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
 
 
 class Base(DeclarativeBase):
@@ -17,7 +23,13 @@ class Base(DeclarativeBase):
             result[column.name.removeprefix(ignore_prefix)] = value
         return result
 
-    pass
+
+stop_to_list_table = Table(
+    "stop_to_list_table",
+    Base.metadata,
+    Column("stops", ForeignKey("stops.stop_id")),
+    Column("stoplists", ForeignKey("stoplists.list_id")),
+)
 
 
 class Stop(Base):
@@ -51,6 +63,39 @@ class User(Base, UserMixin):
     name: Mapped[Optional[str]] = mapped_column(String(100))
     username: Mapped[str] = mapped_column(String(100))
     password: Mapped[str] = mapped_column(String(500))
+    fav_stop_list: Mapped[List["StopList"]] = relationship(
+        cascade="all", lazy='subquery')
 
     def get_id(self):
         return str(self.user_id)
+
+    def get_lists_dict(self, collect_stop=["stop_id", "stop_name", "stop_lat",
+                                           "stop_lon", "transport_type"]):
+        result = []
+        for list in self.fav_stop_list:
+            result.append(list.to_dict(collect_stop))
+        return result
+
+
+class StopList(Base):
+    __tablename__ = "stoplists"
+    list_id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.user_id"))
+    name: Mapped[str] = mapped_column(String(100), unique=True)
+    stops: Mapped[List["Stop"]] = relationship(secondary=stop_to_list_table,
+                                               lazy='subquery',
+                                               cascade='all')
+
+    def to_dict(self, collect_stop=["stop_id", "stop_name", "stop_lat",
+                                    "stop_lon", "transport_type"],
+                ignore_stops=False):
+        result = {}
+        result["id"] = self.list_id
+        result["name"] = self.name
+        if not collect_stop:
+            return result
+        if not ignore_stops:
+            result["stops"] = []
+            for stop in self.stops:
+                result["stops"].append(stop.to_dict(collect_stop, "stop_"))
+        return result
